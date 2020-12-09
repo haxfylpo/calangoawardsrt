6656e0b437225765e378de767b7e6af2bad38b89
haxfylpo <75734972+haxfylpo@users.noreply.github.com>
2020-12-09 11:13:25 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+require('dotenv').config()
+const Twit = require('twit');
+
+/*
+ * - Insert each Twitter credential inside it's equivalent quote.
+ */
+const config = {
+  consumer_key: 'ck0m2mDOuHJ49U1l8rDpPeBeB',
+  consumer_secret: 'PdUMmWd0I1x51DzZokWQ7sUCMbg9ZJ7PfiLBTlGh1qjvbSM0OC',
+  access_token: '1336311407663525888-WGutH8jQLMIkJHk2Ynhk8hjbuFKk82',
+  access_token_secret: '1vD05SqBeeWz6X8paxOwXdFwCERCRfaACOppgHov9NIK',
+};
+
+/*
+ * - Replace "ex1, ex2" with the words you want to retweet.
+ * - Separate words with commas.
+ * - Keep words inside quotes.
+ * - DO NOT repeat the same word with caps on and off (example: "word,Word,WORD").
+ * - If "example" is between the selected words, the bot will retweet any variation of the word:
+ *   example,Example,EXAMPLE,ExAmPlE...
+ */
+const words = '#calangoawards';
+
+/*
+ * - Change 'mybot' with your bot account '@'.
+ * - Example: if your account '@' is '@twitter_bot', you should write only 'twitter_bot'
+ */
+const screenName = 'clangoawardsbot';
+
+/*
+ * If you are a not a programmer, avoid changing anything on the next lines
+ */
+
+const twit = new Twit(config);
+const stream = twit.stream('statuses/filter', { track: words.split(',').map(w => w.trim()) });
+
+console.log('Bot is starting!');
+try {
+  stream.on('tweet', tweet => {
+    if (tweet.user.screen_name === screenName) return;
+
+    if (!tweet.retweeted_status) {
+      twit.post('statuses/retweet/:id', { id: tweet.id_str }, (err, data) => {
+        if (err || !data.text) {
+          return console.log(`Error retweeting user "@${tweet.user.screen_name}"`);
+        }
+        console.log(`Retweeted user "@${tweet.user.screen_name}":\n"${tweet.text}"\n`);
+      });
+    }
+  });
+} catch (_) {
+  console.log('An error ocurred.');
+  process.exit();
+}
