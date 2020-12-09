6c1436b0940bf792cd788ea5487140b35c6b2c2d
haxfylpo <75734972+haxfylpo@users.noreply.github.com>
2020-12-09 11:28:39 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const Twit = require('twit');
  * - Insert each Twitter credential inside it's equivalent quote.
  */
 const config = {
-  consumer_key: 'ck0m2mDOuHJ49U1l8rDpPeBeB',
-  consumer_secret: 'PdUMmWd0I1x51DzZokWQ7sUCMbg9ZJ7PfiLBTlGh1qjvbSM0OC',
-  access_token: '1336311407663525888-WGutH8jQLMIkJHk2Ynhk8hjbuFKk82',
-  access_token_secret: '1vD05SqBeeWz6X8paxOwXdFwCERCRfaACOppgHov9NIK',
+  consumer_key: 'AsqdQJvmXJWCOmL3caILYuHir',
+  consumer_secret: 'dWVCOy4eEiziTfzfSyWEOQ7CC2fq01i6ODaNSahQZohM8eo0jN',
+  access_token: '1336311407663525888-1PnYD3eHYVKM6DNSOjsAWIZcJXbEZR',
+  access_token_secret: 'ddtibEf9G8kloMzrYaWFCBlD5oit35lhWpQg3k26oIhwe',
 };
 
 /*
